fad25d51ff0094d8494c00371bb3d5385d59698b
Brandon <tuberculosis1287@gmail.com>
2023-08-31 19:45:41 -0300

    tests resuelto

---
+++ src/updateIPAddress.ts
@@ -0,0 +1,12 @@
+import { Contacto, contactos } from "../src/contactos";
+
+const updateIPAddress = (contactos:Contacto[]) => {
+
+    contactos.forEach(contacto => {
+        contacto.ip_address = contacto.ip_address + "." +contacto.id
+    });
+    return contactos
+    }; 
+
+export { updateIPAddress };
+
